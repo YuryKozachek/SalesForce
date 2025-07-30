2662c9ea4bbbd351a2857b7e637f66cc8653b4e3
KozachekYury <show_@mail.ru>
2025-07-30 22:16:35 +0300

    добавил АТ по теме обертки в SaleForce

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,53 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+import pages.CreateAccountPage;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.NewAccountPage;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+public class BaseTest {
+
+    WebDriver driver;
+    SoftAssert softAssert;
+    ChromeOptions chromeOptions;
+    LoginPage loginPage;
+    HomePage homePage;
+    CreateAccountPage createAccountPage;
+    NewAccountPage newAccountPage;
+
+    @BeforeMethod
+    public void setSetting() {
+        softAssert = new SoftAssert();
+        chromeOptions = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("credentials_enable_service", false);
+        chromePrefs.put("profile.password_manager_enabled", false);
+        chromeOptions.setExperimentalOption("prefs", chromePrefs);
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        chromeOptions.addArguments("--disable-infobars");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+        createAccountPage = new CreateAccountPage(driver);
+        newAccountPage = new NewAccountPage(driver);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+        softAssert.assertAll();
+    }
+}
